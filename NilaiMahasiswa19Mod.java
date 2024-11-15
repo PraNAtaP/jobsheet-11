c0c4f35bf3c39270859293cdf46af523f5281533
PraNAtaP <pranatapu08@gmail.com>
2024-11-15 12:25:11 +0700

    Tugas 2

--- NilaiMahasiswa19Mod.java
+++ NilaiMahasiswa19Mod.java
@@ -1,20 +1,38 @@
-public class NilaiMahasiswa19 {
+import java.util.Scanner;
+
+public class NilaiMahasiswa19Mod {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMhs = input.nextInt();
+        System.out.print("Masukkan jumlah tugas/minggu: ");
+        int jumlahTugas = input.nextInt();
+
+        int[][] nilai = new int[jumlahMhs][jumlahTugas];
+        String[] nama = new String[jumlahMhs];
+
+        for (int i = 0; i < jumlahMhs; i++) {
+            input.nextLine();
+            System.out.print("\nMasukkan nama mahasiswa ke-" + (i+1) + ": ");
+            nama[i] = input.nextLine();
+            
+            System.out.println("Masukkan nilai untuk " + nama[i] + ": ");
+            for (int j = 0; j < jumlahTugas; j++) {
+                System.out.print("Minggu ke-" + (j+1) + ": ");
+                nilai[i][j] = input.nextInt();
+            }
+        }
         
-        int[][] nilai = {
-            {20, 19, 25, 20, 10, 0, 10},  // Sari
-            {30, 30, 40, 10, 15, 20, 25}, // Rina
-            {5, 0, 20, 25, 10, 5, 45},    // Yani
-            {50, 0, 7, 8, 0, 30, 60},     // Dwi
-            {15, 10, 16, 15, 10, 10, 5}   // Lusi
-        };
-        String[] nama = {"Sari", "Rina", "Yani", "Dwi", "Lusi"};
         tampilkanNilai(nilai, nama);
         cariNilaiTertinggi(nilai);
         mahasiswaNilaiTertinggi(nilai, nama);
+        
+        input.close();
     }
+    
     static void tampilkanNilai(int[][] nilai, String[] nama) {
-        System.out.println("\n----------- Data Nilai Mahasiswa -----------");
+        System.out.println("\n=== Data Nilai Mahasiswa ===");
         for (int i = 0; i < nilai.length; i++) {
             System.out.print(nama[i] + ": ");
             for (int j = 0; j < nilai[i].length; j++) {
@@ -23,6 +41,7 @@ public class NilaiMahasiswa19 {
             System.out.println();
         }
     }
+    
     static void cariNilaiTertinggi(int[][] nilai) {
         int tertinggi = 0;
         int minggu = 0;
@@ -37,21 +56,22 @@ public class NilaiMahasiswa19 {
         }
         System.out.println("\nNilai tertinggi " + tertinggi + " terdapat pada minggu ke-" + minggu);
     }
+    
     static void mahasiswaNilaiTertinggi(int[][] nilai, String[] nama) {
         int tertinggi = 0;
-        int jmlMhs = 0;
+        int indexMhs = 0;
         int minggu = 0;
         
         for (int i = 0; i < nilai.length; i++) {
             for (int j = 0; j < nilai[i].length; j++) {
                 if (nilai[i][j] > tertinggi) {
                     tertinggi = nilai[i][j];
-                    jmlMhs = i;
+                    indexMhs = i;
                     minggu = j + 1;
                 }
             }
         }
-        System.out.println("Mahasiswa dengan nilai tertinggi adalah " + nama[jmlMhs]);
+        System.out.println("Mahasiswa dengan nilai tertinggi adalah " + nama[indexMhs]);
         System.out.println("Nilai: " + tertinggi + " (Minggu ke-" + minggu + ")");
     }
 }
